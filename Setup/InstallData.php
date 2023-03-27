1790ad1fe1c035ca9c55b20dba2ccfc103e26223
Marina <m.nastsenka@alekseon.com>
2023-03-27 18:11:57 +0200

    100.0.5 remove Use ChatGPT values from front

--- Setup/InstallData.php
+++ Setup/InstallData.php
@@ -118,7 +118,7 @@ class InstallData implements InstallDataInterface
                 'searchable' => false,
                 'filterable' => false,
                 'comparable' => false,
-                'visible_on_front' => true,
+                'visible_on_front' => false,
                 'used_in_product_listing' => true,
                 'unique' => false,
                 'group' => 'ChatGPT',
